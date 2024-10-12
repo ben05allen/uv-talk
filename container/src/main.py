68933b955f93bd63c487a1cce181370cae2bef59
ben05allen <ben05allen@hotmail.com>
2024-10-12 10:58:20 +0900

    small edits

--- container/src/main.py
+++ container/src/main.py
@@ -1,9 +1,10 @@
 import cowsay
 import sys
 
+
 def main():
     python_version = sys.version.split()[0]
-    cowsay.cow(f"Container running python: {python_version}")
+    cowsay.cow(f"Running python: {python_version}")
 
 
 if __name__ == "__main__":
